5e18e669ecab4aa4285289c4e688d0ff63f7470b
eunjiom <norgg021@gmail.com>
2026-02-04 20:46:32 +0900

    lv3: 일정 수정 만들기

--- src/main/java/kr/spartaclub/schedule_api/dto/CreateScheduleRequest.java
+++ src/main/java/kr/spartaclub/schedule_api/dto/CreateScheduleRequest.java
@@ -1,4 +1,13 @@
 package kr.spartaclub.schedule_api.dto;
 
+import jakarta.persistence.Column;
+import lombok.Getter;
+
+@Getter
 public class CreateScheduleRequest {
+
+    private String title;   // 일정 제목
+    private String content; // 일정 내용
+    private String author;  // 작성자명
+    private String password; // 비밀번호
 }
--- src/main/java/kr/spartaclub/schedule_api/dto/UpdateScheduleRequest.java
+++ src/main/java/kr/spartaclub/schedule_api/dto/UpdateScheduleRequest.java
@@ -1,4 +1,12 @@
 package kr.spartaclub.schedule_api.dto;
 
+import lombok.Getter;
+
+@Getter
 public class UpdateScheduleRequest {
+
+    private String title;
+    private String author;
+    private String password;
+
 }
