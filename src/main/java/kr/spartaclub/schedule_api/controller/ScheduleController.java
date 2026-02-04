2e3a9acf232a690a44e6181ad9ed18b1c22f40f7
eunjiom <norgg021@gmail.com>
2026-02-04 20:07:53 +0900

    lv2: 일정 조회 수정

--- src/main/java/kr/spartaclub/schedule_api/controller/ScheduleController.java
+++ src/main/java/kr/spartaclub/schedule_api/controller/ScheduleController.java
@@ -1,4 +1,38 @@
 package kr.spartaclub.schedule_api.controller;
 
+import kr.spartaclub.schedule_api.dto.CreateScheduleRequest;
+import kr.spartaclub.schedule_api.dto.CreateScheduleResponse;
+import kr.spartaclub.schedule_api.dto.GetOneScheduleResponse;
+import kr.spartaclub.schedule_api.entity.Schedule;
+import kr.spartaclub.schedule_api.repository.ScheduleRepository;
+import kr.spartaclub.schedule_api.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+
 public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+    private final ScheduleRepository scheduleRepository;
+
+    @PostMapping("/schedule")
+    public CreateScheduleResponse createSchedule(@RequestBody CreateScheduleRequest request) {
+        return scheduleService.save(request);
+    }
+
+    @GetMapping("/schedule/{scheduleId}")
+    public CreateScheduleResponse getOne(@PathVariable Long scheduleId) {
+        return scheduleService.getOne(scheduleId);
+    }
+
+    @GetMapping("/schedule")
+    public List<CreateScheduleResponse> getAll() {
+        return scheduleService.getAll();
+    }
+
 }
--- src/main/java/kr/spartaclub/schedule_api/service/ScheduleService.java
+++ src/main/java/kr/spartaclub/schedule_api/service/ScheduleService.java
@@ -1,4 +1,80 @@
 package kr.spartaclub.schedule_api.service;
 
+import kr.spartaclub.schedule_api.dto.CreateScheduleRequest;
+import kr.spartaclub.schedule_api.dto.CreateScheduleResponse;
+import kr.spartaclub.schedule_api.entity.Schedule;
+import kr.spartaclub.schedule_api.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+
+    // 저장
+    @Transactional
+    public CreateScheduleResponse save(CreateScheduleRequest request){
+        Schedule schedule = new Schedule(
+                request.getTitle(),
+                request.getContent(),
+                request.getAuthor(),
+                request.getPassword()
+
+        );
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+
+        return new CreateScheduleResponse( //변수 인라인화
+
+            savedSchedule.getId(),
+            savedSchedule.getTitle(),
+                savedSchedule.getContent(),
+                savedSchedule.getAuthor(),
+                savedSchedule.getCreatedAt(),
+                savedSchedule.getModifiedAt()
+        );
+
+
+    }
+
+    // 단 건 조회
+    @Transactional(readOnly = true)
+    public CreateScheduleResponse getOne(Long scheduleId) {
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(
+                () -> new IllegalStateException("없는 일정입니다.")
+        );
+
+        return new CreateScheduleResponse(
+                schedule.getId(),
+                schedule.getTitle(),
+                schedule.getContent(),
+                schedule.getAuthor(),
+                schedule.getCreatedAt(),
+                schedule.getModifiedAt()
+        );
+    }
+
+    // 다 건 조회
+    @Transactional(readOnly = true)
+    public List<CreateScheduleResponse> getAll() {
+        List<Schedule> schedules = scheduleRepository.findAll();
+        List<CreateScheduleResponse> dtos = new ArrayList<>();
+
+        for (Schedule schedule : schedules) {
+            CreateScheduleResponse dto = new CreateScheduleResponse(
+                    schedule.getId(),
+                    schedule.getTitle(),
+                    schedule.getContent(),
+                    schedule.getAuthor(),
+                    schedule.getCreatedAt(),
+                    schedule.getModifiedAt()
+            );
+            dtos.add(dto);
+        }
+        return dtos;
+    }
 }
