3c900392aa10e8682f5016df960d297493578c9c
eunjiom <norgg021@gmail.com>
2026-02-04 15:33:18 +0900

    entity, repository, service 생성

---
+++ src/test/java/kr/spartaclub/schedule_api/ScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.schedule_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
