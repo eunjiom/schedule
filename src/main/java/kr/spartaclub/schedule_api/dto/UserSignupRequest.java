6d1c267523e80c60a2ad7ab63495f47d8f4513a2
eunjiom <norgg021@gmail.com>
2026-02-12 19:40:39 +0900

    UserRepository로 메일 확인 기능 추가

--- src/main/java/kr/spartaclub/schedule_api/dto/UserSignupRequest.java
+++ src/main/java/kr/spartaclub/schedule_api/dto/UserSignupRequest.java
@@ -1,4 +1,6 @@
 package kr.spartaclub.schedule_api.dto;
 
-public class UserSignopRequest {
+public class UserSignupRequest {
+
+
 }
--- src/main/java/kr/spartaclub/schedule_api/repository/UserRepository.java
+++ src/main/java/kr/spartaclub/schedule_api/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package kr.spartaclub.schedule_api.repository;
 
-public interface UserRepository {
+import kr.spartaclub.schedule_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 로그인 할 때 등록된 유저인지 확인
+    Optional<User> findByEmail(String email);
+    // 회원가입 할 때 이미 있는 이메일인지 확인
+    boolean existsByEmail(String email);
 }
